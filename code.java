d2daa6dd8a0e38d8b92d7e621306249f687d3dbe
glagarwal <agarwalgaurav1995@gmail.com>
2018-10-10 01:21:13 -0400

    updated Mapper1 to write only words found in vocab.txt

--- code.java
+++ code.java
@@ -1,6 +1,9 @@
 package td;
 
+import java.io.BufferedReader;
+//import java.io.File;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Matcher;
@@ -24,33 +27,40 @@ import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
- 
 
 public class code extends Configured implements Tool {
 
 	private static final Logger LOG = Logger.getLogger(code.class);
 	static int length = 0;
+	static Path vocab_file;
+	static ArrayList<String> vocabList = new ArrayList<String>();
+
 	public static void main(String[] args) throws Exception {
 
 		Configuration conf = new Configuration();
-		Path input = new Path(args[0]);
-		Path output = new Path(args[1]);
-		
+		vocab_file = new Path(args[0]);
+		Path input = new Path(args[1]);
+		Path output = new Path(args[2]);
 
-
-		FileSystem fs = FileSystem.get(conf);	 
+		FileSystem fs = FileSystem.get(conf);
 		ContentSummary cs = fs.getContentSummary(input);
 		length = (int) cs.getFileCount();
-		System.out.println("************ " +length);
+		// System.out.println("************ " +length);
 
-		//length = new File(args[0]).list().length;
-		//System.out.println(length);
-		
+		// length = new File(args[0]).list().length;
+		// System.out.println(length);
+		BufferedReader br = new BufferedReader(new InputStreamReader(fs.open(vocab_file)));
+		String word = br.readLine();
+		while (word != null) {
+			vocabList.add(word.toLowerCase().trim());
+			word = br.readLine();
+		}
 
 		Job job1 = Job.getInstance(conf, "wordInDoc");
 		job1.setJarByClass(code.class);
 
 		job1.setMapperClass(Map1.class);
+		job1.setCombinerClass(Reduce1.class);
 		job1.setReducerClass(Reduce1.class);
 
 		job1.setOutputKeyClass(Text.class);
@@ -82,8 +92,7 @@ public class code extends Configured implements Tool {
 
 		if (!(job2.waitForCompletion(true)))
 			System.exit(1);
- 
-		
+
 	}
 
 	public int run(String[] args) throws Exception {
@@ -92,7 +101,7 @@ public class code extends Configured implements Tool {
 	}
 
 	public static class Map1 extends Mapper<LongWritable, Text, Text, IntWritable> {
-		
+
 		private final static IntWritable one = new IntWritable(1);
 
 		public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
@@ -103,79 +112,78 @@ public class code extends Configured implements Tool {
 			String line = value.toString();
 			line = line.toLowerCase();
 
-			Pattern p = Pattern.compile("\\w+", Pattern.CASE_INSENSITIVE);
-			Matcher m = p.matcher(line);
+			Pattern p = Pattern.compile("([\\w-]+)", Pattern.CASE_INSENSITIVE);
+			// Matcher m = p.matcher(line);
+			String words[] = line.split(" ");
 
-			
-			while (m.find()) {
-				String s = m.group();
-				/*
-				 * if (s.isEmpty()) { continue; }
-				 */
-				s = s + "@" + filePathString;
-				Text addThis = new Text(s);
-				context.write(addThis, one);
+			for (String s : words) {
+				Matcher m = p.matcher(s);
+				if (!vocabList.contains(s.toLowerCase()) && m.find())
+					s = m.group();
+				if (vocabList.contains(s.toLowerCase())) {
+					s = s + "@" + filePathString;
+					Text addThis = new Text(s);
+					context.write(addThis, one);
+				}
 			}
 		}
 	}
 
 	public static class Reduce1 extends Reducer<Text, IntWritable, Text, IntWritable> {
 		@Override
-		public void reduce(Text word, Iterable<IntWritable> counter, Context context) throws IOException, InterruptedException {
-			int sum = 0; 
+		public void reduce(Text word, Iterable<IntWritable> counter, Context context)
+				throws IOException, InterruptedException {
+			int sum = 0;
 			for (IntWritable count : counter) {
-				sum ++;
+				sum += count.get();
 			}
 			context.write(word, new IntWritable(sum));
 		}
-	} 
+	}
 
-	public static class Map2 extends Mapper<LongWritable, Text, Text, Text> { 
-		public void map(LongWritable key, Text value, Context context)
-				throws IOException, InterruptedException {
+	public static class Map2 extends Mapper<LongWritable, Text, Text, Text> {
+		public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
 			String string = value.toString();
 			String[] parseValues = string.split("\t");
-			String[] parseNames  = parseValues[0].split("@");
- 
+			String[] parseNames = parseValues[0].split("@");
+
 			Text word = new Text(parseNames[0]);
-			Text docAndCount = new Text(parseNames[1]+"="+parseValues[1]);
-			//System.out.println(word+", "+ docAndCount);
+			Text docAndCount = new Text(parseNames[1] + "=" + parseValues[1]);
+			// System.out.println(word+", "+ docAndCount);
 			context.write(word, docAndCount);
- 
+
 		}
 	}
 
-	
-
-	public static class Reduce2 extends
-			Reducer<Text, Text, Text, Text> {
+	public static class Reduce2 extends Reducer<Text, Text, Text, Text> {
 		@Override
-		public void reduce(Text word, Iterable<Text> wordCount, Context context) throws IOException, InterruptedException {
+		public void reduce(Text word, Iterable<Text> wordCount, Context context)
+				throws IOException, InterruptedException {
 
 			String wordString = word.toString();
 			List<String> cache = new ArrayList<String>();
 			int totalDocs = 0;
-			for (Text count : wordCount) { 
+			for (Text count : wordCount) {
 				String string = count.toString();
-				cache.add(string);  
-				totalDocs++; 
-			} 
-			
-			for (String string : cache) { 
-				String[] parseValues = string.split("="); 
-				//System.out.println(string);
+				cache.add(string);
+				totalDocs++;
+			}
+
+			for (String string : cache) {
+				String[] parseValues = string.split("=");
+				// System.out.println(string);
 				String docName = parseValues[0];
-				double wordcount = Double.valueOf(parseValues[1]); 
+				double wordcount = Double.valueOf(parseValues[1]);
 				double tf = wordcount;
-				double preidf = ((double) length)/((double) totalDocs);
-				double idf = Math.log(preidf)/Math.log(2);
-				double tfidf = tf*idf;
-				
-				//System.out.println(wordString+"\t"+docName +"\t"+ tfidf);
-				Text wordAndDoc = new Text(wordString+"\t"+docName);
+				double preidf = ((double) length) / ((double) totalDocs);
+				double idf = Math.log(preidf) / Math.log(2);
+				double tfidf = tf * idf;
+
+				// System.out.println(wordString+"\t"+docName +"\t"+ tfidf);
+				Text wordAndDoc = new Text(wordString + "\t" + docName);
 				Text wordAndCount = new Text(String.valueOf(tfidf));
 				context.write(wordAndDoc, wordAndCount);
-			} 
+			}
 		}
 	}
 }
--- code_norm.java
+++ code_norm.java
@@ -1,6 +1,9 @@
 package td;
- 
+
+import java.io.BufferedReader;
+//import java.io.File;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Matcher;
@@ -24,28 +27,34 @@ import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
- 
 
 public class code_norm extends Configured implements Tool {
 
 	private static final Logger LOG = Logger.getLogger(code_norm.class);
 	static int length = 0;
+	static Path vocab_file;
+	static ArrayList<String> vocabList = new ArrayList<String>();
+
 	public static void main(String[] args) throws Exception {
 
 		Configuration conf = new Configuration();
-		Path input = new Path(args[0]);
-		Path output = new Path(args[1]);
-		
+		vocab_file = new Path(args[0]);
+		Path input = new Path(args[1]);
+		Path output = new Path(args[2]);
 
-
-		FileSystem fs = FileSystem.get(conf);	 
+		FileSystem fs = FileSystem.get(conf);
 		ContentSummary cs = fs.getContentSummary(input);
 		length = (int) cs.getFileCount();
-		System.out.println("************ " +length);
+		// System.out.println("************ " +length);
 
-		//length = new File(args[0]).list().length;
-		//System.out.println(length);
-		
+		// length = new File(args[0]).list().length;
+		// System.out.println(length);
+		BufferedReader br = new BufferedReader(new InputStreamReader(fs.open(vocab_file)));
+		String word = br.readLine();
+		while (word != null) {
+			vocabList.add(word.toLowerCase().trim());
+			word = br.readLine();
+		}
 
 		Job job1 = Job.getInstance(conf, "wordInDoc");
 		job1.setJarByClass(code_norm.class);
@@ -82,7 +91,7 @@ public class code_norm extends Configured implements Tool {
 
 		if (!(job2.waitForCompletion(true)))
 			System.exit(1);
-		
+
 		Job job3 = Job.getInstance(conf, "finalTFIDF");
 		job3.setJarByClass(code_norm.class);
 
@@ -100,7 +109,7 @@ public class code_norm extends Configured implements Tool {
 
 		if (!(job3.waitForCompletion(true)))
 			System.exit(1);
-		
+
 	}
 
 	public int run(String[] args) throws Exception {
@@ -109,7 +118,7 @@ public class code_norm extends Configured implements Tool {
 	}
 
 	public static class Map1 extends Mapper<LongWritable, Text, Text, IntWritable> {
-		
+
 		private final static IntWritable one = new IntWritable(1);
 
 		public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
@@ -123,119 +132,121 @@ public class code_norm extends Configured implements Tool {
 			Pattern p = Pattern.compile("\\w+", Pattern.CASE_INSENSITIVE);
 			Matcher m = p.matcher(line);
 
-			
 			while (m.find()) {
 				String s = m.group();
 				/*
 				 * if (s.isEmpty()) { continue; }
 				 */
-				s = s + "@" + filePathString;
-				Text addThis = new Text(s);
-				context.write(addThis, one);
+				if (vocabList.contains(s.toLowerCase())) {
+					s = s + "@" + filePathString;
+					Text addThis = new Text(s);
+					context.write(addThis, one);
+				}
 			}
 		}
 	}
 
 	public static class Reduce1 extends Reducer<Text, IntWritable, Text, IntWritable> {
 		@Override
-		public void reduce(Text word, Iterable<IntWritable> counter, Context context) throws IOException, InterruptedException {
-			int sum = 0; 
+		public void reduce(Text word, Iterable<IntWritable> counter, Context context)
+				throws IOException, InterruptedException {
+			int sum = 0;
 			for (IntWritable count : counter) {
-				sum ++;
+				sum++;
 			}
 			context.write(word, new IntWritable(sum));
 		}
 	}
 
-	public static class Map2 extends Mapper<LongWritable, Text, Text, Text> { 
-		public void map(LongWritable key, Text value, Context context)
-				throws IOException, InterruptedException {
+	public static class Map2 extends Mapper<LongWritable, Text, Text, Text> {
+		public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
 			String string = value.toString();
 			String[] parseValues = string.split("\t");
-			String[] parseNames  = parseValues[0].split("@");
- 
+			String[] parseNames = parseValues[0].split("@");
+
 			Text docName = new Text(parseNames[1]);
-			Text wordAndCount = new Text(parseNames[0]+"="+parseValues[1]);
+			Text wordAndCount = new Text(parseNames[0] + "=" + parseValues[1]);
 			context.write(docName, wordAndCount);
- 
+
 		}
 	}
- 
-	public static class Reduce2 extends
-			Reducer<Text, Text, Text, Text> {
+
+	public static class Reduce2 extends Reducer<Text, Text, Text, Text> {
 		@Override
-		public void reduce(Text docName, Iterable<Text> wordCount, Context context) throws IOException, InterruptedException {
-			
+		public void reduce(Text docName, Iterable<Text> wordCount, Context context)
+				throws IOException, InterruptedException {
+
 			String docname = docName.toString();
 			List<String> cache = new ArrayList<String>();
 			int totalWords = 0;
 			for (Text count : wordCount) {
-				
+
 				String string = count.toString();
-				cache.add(string); 
+				cache.add(string);
 				String[] parseValues = string.split("=");
-				totalWords+= Integer.parseInt(parseValues[1]); 	
-			} 
-			for (String string : cache) { 
-				String[] parseValues = string.split("="); 
-				//System.out.println(string);
+				totalWords += Integer.parseInt(parseValues[1]);
+			}
+			for (String string : cache) {
+				String[] parseValues = string.split("=");
+				// System.out.println(string);
 				String totalWordsS = String.valueOf(totalWords);
-				Text docAndTotal = new Text(docname+"="+ totalWordsS);
-				Text wordAndCount = new Text(parseValues[0]+"="+parseValues[1]);
+				Text docAndTotal = new Text(docname + "=" + totalWordsS);
+				Text wordAndCount = new Text(parseValues[0] + "=" + parseValues[1]);
 				context.write(docAndTotal, wordAndCount);
-			} 
-			
+			}
+
 		}
 	}
-	
-	public static class Map3 extends Mapper<LongWritable, Text, Text, Text> { 
+
+	public static class Map3 extends Mapper<LongWritable, Text, Text, Text> {
 
 		public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
 			String string = value.toString();
 			String[] parseValues = string.split("\t");
-			String[] docAndLength  = parseValues[0].split("=");
-			String[] wordAndCount  = parseValues[1].split("=");
-			
-			//System.out.println(wordAndCount[0]+": "+docAndLength[0]+"="+ wordAndCount[1]+"/"+docAndLength[1] );
-			
+			String[] docAndLength = parseValues[0].split("=");
+			String[] wordAndCount = parseValues[1].split("=");
+
+			// System.out.println(wordAndCount[0]+": "+docAndLength[0]+"="+
+			// wordAndCount[1]+"/"+docAndLength[1] );
+
 			Text word = new Text(wordAndCount[0]);
-			Text docAndRatio = new Text(docAndLength[0]+"="+ wordAndCount[1]+"/"+docAndLength[1]);
+			Text docAndRatio = new Text(docAndLength[0] + "=" + wordAndCount[1] + "/" + docAndLength[1]);
 			context.write(word, docAndRatio);
- 
+
 		}
 	}
 
-	public static class Reduce3 extends
-			Reducer<Text, Text, Text, Text> {
+	public static class Reduce3 extends Reducer<Text, Text, Text, Text> {
 		@Override
-		public void reduce(Text word, Iterable<Text> wordCount, Context context) throws IOException, InterruptedException {
+		public void reduce(Text word, Iterable<Text> wordCount, Context context)
+				throws IOException, InterruptedException {
 
 			String wordString = word.toString();
 			List<String> cache = new ArrayList<String>();
 			int totalDocs = 0;
-			for (Text count : wordCount) { 
+			for (Text count : wordCount) {
 				String string = count.toString();
-				cache.add(string);  
-				totalDocs++; 
-			} 
-			
-			for (String string : cache) { 
-				String[] parseValues = string.split("="); 
+				cache.add(string);
+				totalDocs++;
+			}
+
+			for (String string : cache) {
+				String[] parseValues = string.split("=");
 				String[] parseTF = parseValues[1].split("/");
-				//System.out.println(string);
+				// System.out.println(string);
 				String docName = parseValues[0];
 				double wordcount = Double.valueOf(parseTF[0]);
 				double docLengh = Double.valueOf(parseTF[1]);
-				double tf = wordcount/docLengh;
-				double preidf = ((double) length)/((double) totalDocs);
-				double idf = Math.log(preidf)/Math.log(2);
-				double tfidf = tf*idf;
-				
-				//System.out.println(wordString+"\t"+docName +"\t"+ tfidf);
-				Text wordAndDoc = new Text(wordString+"\t"+docName);
+				double tf = wordcount / docLengh;
+				double preidf = ((double) length) / ((double) totalDocs);
+				double idf = Math.log(preidf) / Math.log(2);
+				double tfidf = tf * idf;
+
+				// System.out.println(wordString+"\t"+docName +"\t"+ tfidf);
+				Text wordAndDoc = new Text(wordString + "\t" + docName);
 				Text wordAndCount = new Text(String.valueOf(tfidf));
 				context.write(wordAndDoc, wordAndCount);
-			} 
+			}
 		}
 	}
 }
